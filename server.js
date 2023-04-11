e5f5f7730adbec7b0e01f270f70a1bda7b5aab59
Anirudh Pratap Singh <36331702+flutterninja9@users.noreply.github.com>
2023-04-11 21:27:01 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 200,
+    message: 'OK',
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
